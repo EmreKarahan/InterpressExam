86eaeb82c580ffb71cc782d577622133644732e9
Emre Karahan <emre.karahan@dogangazetecilik.com.tr>
2017-07-14 16:35:50 +0300

    ok

---
+++ InterpressExam.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace InterpressExam.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
