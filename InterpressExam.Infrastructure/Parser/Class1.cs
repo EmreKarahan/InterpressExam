4401da258652c522237c808095888bcfc59788b3
Emre Karahan <emre.karahan@dogangazetecilik.com.tr>
2017-07-14 16:51:55 +0300

    first commit

---
+++ InterpressExam.Infrastructure/Parser/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+
+namespace InterpressExam.Infrastructure.Parser
+{
+    public class Class1
+    {
+        private string XmlPath = "C:\\XML";
+        var xml = XDocument.Load("http://www.dreamincode.net/forums/xml.php?showuser=1253");
+
+    }
+}
