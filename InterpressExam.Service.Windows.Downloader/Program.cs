1441d6c812d3200d1b8d191f3a82aa4768ddd7a0
Emre Karahan <emre.karahan@gmail.com>
2017-07-16 14:20:51 +0300

    ok

---
+++ InterpressExam.Service.Windows.Downloader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace InterpressExam.Service.Windows.Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aa = GetFeed("http://www.milliyet.com.tr/rss/rssNew/magazinRss.xml");
+
+        }
+
+        public static SyndicationFeed GetFeed(string uri)
+        {
+            if (string.IsNullOrEmpty(uri)) return null;
+            var rssFormatter = new Rss20FeedFormatter(); // for Atom you can use Atom10FeedFormatter()
+            var xmlReader = XmlReader.Create(uri);
+            rssFormatter.ReadFrom(xmlReader);
+            return rssFormatter.Feed;
+        }
+    }
+}
